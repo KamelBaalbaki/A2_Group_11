41ff33853ecc4a8fea095ba9d9145fa7dbfa0c5a
KamelBaalbaki <baalbaki.kamel@gmail.com>
2025-11-24 18:39:23 -0500

    Added Card Component

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function Card({ children, style }) {
+return (
+    <View style={[styles.card, style]}>{children}</View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 14,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.06,
+    shadowRadius: 10,
+    elevation: 3,
+  }
+});
