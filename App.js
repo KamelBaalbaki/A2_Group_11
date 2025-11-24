a533848e239c45f56c9778ec205f837a6745359e
KamelBaalbaki <baalbaki.kamel@gmail.com>
2025-11-24 17:55:31 -0500

    Initializing Project

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+import AboutScreen from './screens/AboutScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+  <NavigationContainer>
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+  <Stack.Screen name="Main" component={MainScreen} />
+  <Stack.Screen name="About" component={AboutScreen} />
+  </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
