6c488c076d1578cfb947cac2ecdd153d49bfbe88
KamelBaalbaki <baalbaki.kamel@gmail.com>
2025-11-24 18:37:46 -0500

    Added An Array for Currencies

---
+++ data/currencies.js
@@ -0,0 +1,16 @@
+export const CURRENCY_LIST = [
+  { label: "CAD – Canadian Dollar", value: "CAD" },
+  { label: "USD – US Dollar", value: "USD" },
+  { label: "EUR – Euro", value: "EUR" },
+  { label: "GBP – British Pound", value: "GBP" },
+  { label: "AUD – Australian Dollar", value: "AUD" },
+  { label: "JPY – Japanese Yen", value: "JPY" },
+  { label: "CHF – Swiss Franc", value: "CHF" },
+  { label: "CNY – Chinese Yuan", value: "CNY" },
+  { label: "INR – Indian Rupee", value: "INR" },
+  { label: "SAR – Saudi Riyal", value: "SAR" },
+  { label: "AED – UAE Dirham", value: "AED" },
+  { label: "TRY – Turkish Lira", value: "TRY" },
+  { label: "MXN – Mexican Peso", value: "MXN" },
+  { label: "BRL – Brazilian Real", value: "BRL" }
+];
